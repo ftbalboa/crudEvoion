e766e8d8764bf57a4bb76e333baa30b7e89f6f35
Francisco Balboa <ftbalboa@Franciscos-MacBook-Air.local>
2021-09-17 03:14:34 -0300

    feed redirect and logout

--- pages/feed/index.js
+++ pages/feed/index.js
@@ -1,8 +1,17 @@
-import { useContext } from "react";
+import { useRouter } from "next/dist/client/router";
+import { useContext, useEffect } from "react";
+import { userContext } from "../_app";
 
 export default function Feed (){
     const {user, setUser} = useContext(userContext);
+    const router = useRouter();
+    useEffect(() => {
+        !user && router.push('/', undefined, { shallow: true });
+    }, [user])
     return (
+        <div>
         <h1>Hi from Feed</h1>
+        <button onClick={()=> setUser(false)}>Logout</button>
+        </div>
     )
 }
